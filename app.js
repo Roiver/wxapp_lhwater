f868303289fb02a4a5e30615093b6fd629352c19
Roiver <914520073@qq.com>
2022-04-28 13:54:21 +0800

    index 页面修改

--- app.js
+++ app.js
@@ -14,6 +14,40 @@ App({
     })
   },
   globalData: {
-    userInfo: null
-  }
+    userInfo: null,
+    host: 'https://lhwater.dg88888888.com',
+  },
+  wxRequest: function(url, data, successCB, type) {
+    var that = this;
+    var requestUrl = that.globalData.host + '/user_api/' + url;
+    var requestMethod = type ? type : 'POST';
+    var requestConType = 'application/x-www-form-urlencoded';
+    // if (that.fm.gain('token') && !data.shop_token) data.token = that.fm.gain('token');
+    if (typeof url != 'string') {
+      requestUrl = url.url;
+      requestMethod = url.method;
+      requestConType = url.conType;
+    }
+    wx.request({
+      url: requestUrl,
+      header: {
+        'content-type': requestConType
+      },
+      data: data,
+      method: requestMethod,
+      success: function(res) {
+        if (res.data.code == -5) {
+          console.log('用户未授权登录');
+        } else if (res.data.code == -101) {
+          console.log(res.data.code);
+        } else {
+          typeof successCB == "function" && successCB(res);
+        }
+      },
+      fail: function(err) {
+        console.log(err);
+      }
+    })
+  },
+
 })
